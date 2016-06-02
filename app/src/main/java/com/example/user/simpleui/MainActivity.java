22b0da1c9e068cee680c9a081f2c9d45e0c57a9f
jackman7766 <jackman7766@gmail.com>
2016-06-02 22:17:06 +0800

    Project_2

--- app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -3,15 +3,83 @@ package com.example.user.simpleui;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView textview;
+    EditText editText;
+    RadioGroup radioGroup;
+    CheckBox checkBox;
+    String selectedSex = "Male";
+    String name = "";
+    String sex = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textview = (TextView)findViewById(R.id.TextView);
-        textview.setText("Hello TextView");
+        textview = (TextView) findViewById(R.id.TextView);
+        editText = (EditText) findViewById(R.id.editText);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        checkBox = (CheckBox) findViewById(R.id.checkBox);
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    click(v);
+                    return true;
+                }
+                return false;
+            }
+        });
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedID) {
+                if (checkedID == R.id.maleRadioButton) {
+                    selectedSex = "Male";
+                } else if (checkedID == R.id.femaleRadioButton2) {
+                    selectedSex = "Female";
+                }
+            }
+        });
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                changeTextView();
+            }
+        });
+
     }
-}
+        public void click(View view) {
+               name = editText.getText().toString();
+
+                sex = selectedSex;
+
+                changeTextView();
+
+                editText.setText("");
+            }
+            public void changeTextView()
+            {
+               if(name.equals(""))
+                   return;
+                if (checkBox.isChecked())
+               {
+                  textview.setText(name);
+               }
+                else
+               {
+                   String content = name + "sex: " + selectedSex;
+
+                   textview.setText(content);
+               }
+
+            }
+        }
+
