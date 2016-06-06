db9ad276786c97bc52c3a76ec85da5ae8149b82c
jackman7766 <jackman7766@gmail.com>
2016-06-06 20:39:22 +0800

    Project_class_3

--- app/src/main/java/com/example/user/simpleui/MainActivity.java
+++ app/src/main/java/com/example/user/simpleui/MainActivity.java
@@ -5,29 +5,45 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
-    TextView textview;
+    TextView textView;
     EditText editText;
     RadioGroup radioGroup;
     CheckBox checkBox;
-    String selectedSex = "Male";
-    String name = "";
-    String sex = "";
-
+    ListView listView;
+    ArrayList<Order> orders = new ArrayList<>();
+//    String selectedSex = "Male";
+//    String name = "";
+//    String sex = "";
+String drinkName = "black tea";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textview = (TextView) findViewById(R.id.TextView);
+        textView = (TextView) findViewById(R.id.TextView);
         editText = (EditText) findViewById(R.id.editText);
         radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
         checkBox = (CheckBox) findViewById(R.id.checkBox);
+        listView = (ListView)findViewById(R.id.listView);
+
+        setupListView();
+
+        setupListView();
         editText.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
@@ -41,45 +57,75 @@ public class MainActivity extends AppCompatActivity {
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedID) {
-                if (checkedID == R.id.maleRadioButton) {
-                    selectedSex = "Male";
-                } else if (checkedID == R.id.femaleRadioButton2) {
-                    selectedSex = "Female";
-                }
+//                if (checkedID == R.id.maleRadioButton) {
+//                    selectedSex = "Male";
+//                } else if (checkedID == R.id.femaleRadioButton2) {
+//                    selectedSex = "Female";
+//                }
+                RadioButton radioButton = (RadioButton) findViewById(checkedID);
+                drinkName = radioButton.getText().toString();
             }
         });
-        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                changeTextView();
-            }
-        });
+//        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                changeTextView();
+//            }
+//        });
 
     }
+
+    void setupListView()
+    {
+//        String[] data = new String[]{"123", "456","789","Hello","ListView","Hi"};
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, drinks);
+        List<Map<String,String>> data = new ArrayList<>();
+
+        for(int i = 0; i < orders.size(); i++)
+        {
+            Order order = orders.get(i);
+            Map<String,String> item = new HashMap<>();
+
+            item.put("note", order.note); //(key , value)
+            item.put("drinkName", order.drinkName);
+
+            data.add(item);
+        }
+        String[] from = {"note", "drinkName"};
+        int[] to = {R.id.noteTextView, R.id.drinkNameTextView};
+
+        SimpleAdapter adapter = new SimpleAdapter(this, data, R.layout.listview_oder_item, from, to);
+        listView.setAdapter(adapter);
+    }
+
         public void click(View view) {
-               name = editText.getText().toString();
+               String note = editText.getText().toString();
+//                sex = selectedSex;
+//                changeTextView();
 
-                sex = selectedSex;
-
-                changeTextView();
-
+            Order order = new Order();
+            order.note = note;
+            order.drinkName = drinkName;
+            orders.add(order);
+                textView.setText(drinkName);
                 editText.setText("");
+                setupListView();
             }
-            public void changeTextView()
-            {
-               if(name.equals(""))
-                   return;
-                if (checkBox.isChecked())
-               {
-                  textview.setText(name);
-               }
-                else
-               {
-                   String content = name + "sex: " + selectedSex;
-
-                   textview.setText(content);
-               }
-
-            }
+//            public void changeTextView()
+//            {
+//               if(name.equals(""))
+//                   return;
+//                if (checkBox.isChecked())
+//               {
+//                  textview.setText(name);
+//               }
+//                else
+//               {
+//                   String content = name + "sex: " + selectedSex;
+//
+//                   textview.setText(content);
+//               }
+//
+//            }
         }
 
